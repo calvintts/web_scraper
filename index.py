33b092daa489f81e7fcb0f4a66118205a36759ed
calvintts <calvinteytaisoon@gmail.com>
2018-07-24 00:17:15 -0700

    scrapped title image and author

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import array
+import json
 
 myurl = "https://www.phonearena.com/"
 
@@ -8,6 +10,13 @@ html_page = something.read()
 something.close()
 #html parsing
 soup_page = soup(html_page, "html.parser")
-#grabs each product
 items = soup_page.findAll("div",{"class":"ln-item"})
-print(len(items))
+array = []
+#grabs each product
+for item in items:
+    data = {}
+    data['title'] = item.div.h3.a.text
+    data['image'] = item.div.img["src"]
+    data['author'] = item.findAll("span",{"class":"author"})
+    array.append(data)
+print(array[0])
