e47b44a07dee85fd7e1c43a18585d78082ee38bd
calvintts <calvintts@mail.fresnostate.edu>
2018-07-24 12:45:18 -0700

    scrapping done but return value needed configuration

--- index.py
+++ index.py
@@ -3,20 +3,75 @@ from bs4 import BeautifulSoup as soup
 import array
 import json
 
-myurl = "https://www.phonearena.com/"
+def scrap_item(url):
+    url_req = uReq(url)
+    url_read = url_req.read()
+    url_req.close()
+    temp = {}
+    page = soup(url_read,"html.parser")
+    page_info = page.find("div",{"id":"article_item"})
+    content = page_info.find("div",{"id":"article-content"})
+    temp['text'] = content.text.strip()
+    # print(temp['text'])
+    if(content.find_all('img')):
+        temp['image']= []
+        for pic in content.find_all('img'):
+            temp['image'].append(pic.get('src'))
+    if(content.find_all('iframe')):
+        temp['videos']= []
+        for vid in content.find_all('iframe'):
+            temp['videos'].append(vid.get('src'))
+    if(page_info.h1):
+        temp['title'] = page_info.h1.text
+    if(page_info.find("p",{"class":"s_date"}).time):
+        temp['date'] = page_info.find("p",{"class":"s_date"}).time.text
+    if(page_info.find("p",{"class":"s_date"})):
+        dummy = page_info.find("p",{"class":"s_date"}).text.split("by")
+        temp['author'] = dummy[len(dummy)-1].strip()
+    return temp
+
+myurl = "https://www.phonearena.com"
 
 something = uReq(myurl)
 html_page = something.read()
 something.close()
 #html parsing
 soup_page = soup(html_page, "html.parser")
-items = soup_page.findAll("div",{"class":"ln-item"})
+articles = soup_page.findAll("div",{"class":["article_home"]})
 array = []
-#grabs each product
-for item in items:
+
+#grabs each article from homepage
+for article in articles:
     data = {}
-    data['title'] = item.div.h3.a.text
-    data['image'] = item.div.img["src"]
-    data['author'] = item.findAll("span",{"class":"author"})
-    array.append(data)
-print(array[0])
+    data['title'] = article.div.h3.a.text
+    #data['image'] = article.div.img["src"]
+    data['link']= myurl + article.a["href"]
+    #data['info']= scrap_item(data['link'])
+    try:
+        data['information']=scrap_item(data['link'])
+        array.append(data)
+    except:
+        print(data['title']+ 'page is skipped')
+
+
+for item in array:
+    print(item)
+    print()
+    print()
+    
+# testitem = array[0]
+# print(testitem)
+
+
+
+
+    # details_page = details.read()
+    # details.close()
+    # page_info = soup(details_page,"html.parser")
+    # page_content = page_info.find("div",{"class":"s_static"})
+    # data['image'] = page_content.div.img["src"]
+    # data['text'] = page_content.find("div",{"id":"article-content"}).find(recursive=False).text
+    # date_and_author_box = page_info.find("p",{"class":"s_date"})
+    # data['date'] = date_and_author_box.time.text
+    # dummy = date_and_author_box.text.split("by")
+    # data['author'] = dummy[len(dummy)-1].strip()
